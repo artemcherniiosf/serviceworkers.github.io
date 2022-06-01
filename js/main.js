b4438df282db56286699a4cd6fe3de9d0834b907
Artem Chernii <artem.chernii@osf.digital>
2022-06-01 16:35:54 +0100

    refactor: restructure files

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Make sure service workers are supported
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('sw_cached_pages.js')
-      .then((reg) => {
-        console.log('Service worker registered', reg);
-      })
-      .catch((err) => `Service worker not registered: ${err}`);
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('sw_cached_pages.js')
+//       .then((reg) => {
+//         console.log('Service worker registered', reg);
+//       })
+//       .catch((err) => `Service worker not registered: ${err}`);
+//   });
+// }
